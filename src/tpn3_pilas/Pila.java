21653530d9d1b47bbc8773a4ed167435d974bfee
Barito06 <aguerocalderonhectorjoaquin@gmail.com>
2024-04-18 11:22:10 -0300

    invierteEficiente

--- src/tpn3_pilas/Pila.java
+++ src/tpn3_pilas/Pila.java
@@ -1,21 +1,19 @@
 package tpn3_pilas;
 
-import java.util.Scanner;
-
 /*
 Implemente la clase pila, se trata de una pila de enteros, la misma debe contener
 los métodos: push(), pop(), pilaVacia(), PilaLlena(), verElemento()
 Cree un objeto de la clase y verifique sus métodos.
  */
 public class Pila {
-    
-    private int n = 10;
+
+    private int n;
     private int[] pila;
     private int tope = -1;
-    private Scanner Leer = new Scanner(System.in);
-    
-    public Pila() {
-        pila = new int[n]; 
+
+    public Pila(int n) {
+        this.n = n;
+        pila = new int[n];
     }
 //getters
     public int getN() {
@@ -27,23 +25,26 @@ public class Pila {
     public int getTope() {
         return tope;
     }
- 
-    public void push(int x) {
+
+    public int push(int x) {
         if (tope < n - 1) {
             tope++;
             pila[tope] = x;
-            //System.out.println(pila[tope]);
+            //System.out.println("El numero introducido es: " + x);
         } else {
             System.out.println("La estructura esta completa, no se puede insertar");
         }
+        return x;
     }
 
-    public void pop() {
+    public int pop() {
         if (tope >= 0) {
+            //System.out.println("El numero retirado es: " + tope);
             tope--;
         } else {
             System.out.println("La estructura esta vacia, no se puede eliminar");
         }
+        return tope;
     }
 
     public boolean pilaVacia() {
@@ -51,46 +52,65 @@ public class Pila {
     }
 
     public boolean pilaLlena() {
-        return tope == n-1;
+        return tope == n - 1;
     }
 
     public int verElemento() {
-        return pila[tope];
+        int var = pila[tope];
+        return var;
     }
-    
-    public void invierteSimple(){
-    
-        Pila p2 = new Pila();
-        Pila p3 = new Pila(); 
-        
-        for (int j = 0; j < n; j++) {
-            int s = verElemento();
-            pop();
-            p2.push(s);
+
+    public void MostrarPila() {
+        for (int i = 0; i < n; i++) {
+            System.out.println(pila[i]);
         }
-        
-        for (int j = 0; j < n; j++) {
-            int a = p2.verElemento();
+    }
+
+    public void invierteSimple(Pila p1, Pila p2, Pila p3) {
+
+        while (!p1.pilaVacia()) {
+            p2.push(p1.verElemento());
+            p1.pop();
+        }
+        while (!p2.pilaVacia()) {
+            p3.push(p2.verElemento());
             p2.pop();
-            p3.push(a);
         }
-        for (int j = 0; j < n; j++) {
-            int b = p3.verElemento();
+        while (!p3.pilaVacia()) {
+            p1.push(p3.verElemento());
             p3.pop();
-            push(b);
-        }    
-    }
-    
-    public void invierteEficiente(){
-    int aux;
-    Pila pAux =new Pila();
-    
-    aux = verElemento();
-    for (int j = 0; j < n; j++) {
-            int s = verElemento();
-            pop();
-            pAux.push(s);
         }
-    
+    }
+
+    public void invierteEficiente(Pila p1, Pila pAux){
+        /*
+        Voy a hacer de cuenta que tengo un arreglo p1 con los siguientes valores
+        [1,2,3,4,5]
+        */
+    int aux;    
+    // asignamos a la variable auxiliar el tope de la pila 1 (Segun el ejemplo el valor seria 5).
+        for (int k = 0; k < n; k++) {
+            
+    aux = verElemento();
+    // Pasamos todos los valores de la pila 1 a la pila 2 y en el camino vaciamos la pila 1.
+    // Nos quedaria p1[0,0,0,0,0] y pAux[5,4,3,2,1]    
+    for (int i = 0; i < n; i++) {
+        if(!pAux.pilaLlena()){
+            pAux.push(p1.verElemento());
+            p1.pop();
+        }
+    }
+    //Ahora el valor de la variable aux se lo introducimos en la p1(pila original)
+            p1.push(aux);
+    // Aqui se devuelven los valores a la pila inicial, con la diferencia que solo se ingresan 4 de los 5 valores
+    // por los que la pila1 queda como p1[5,1,2,3,4]
+        
+        for (int j = 0; j < n; j++) {
+            if(!p1.pilaLlena()){
+            p1.push(pAux.verElemento());
+            pAux.pop();
+        }
+        }
+        }
     }
 }
--- src/tpn3_pilas/TPn3_Pilas.java
+++ src/tpn3_pilas/TPn3_Pilas.java
@@ -2,9 +2,14 @@ package tpn3_pilas;
 public class TPn3_Pilas {
 
     public static void main(String[] args) {
-        Pila p1 = new Pila();
-        p1.push(0);
-        p1.push(1);
+        Pila p1 = new Pila(10);
+        Pila p2 = new Pila(10);
+        Pila p3 = new Pila(10);
+        Pila p4 = new Pila(5);
+        Pila p5 = new Pila(5);
+        Pila p6 = new Pila(10);
+
+        p1.push(1);        
         p1.push(2);
         p1.push(3);
         p1.push(4);
@@ -13,17 +18,17 @@ public class TPn3_Pilas {
         p1.push(7);
         p1.push(8);
         p1.push(9);
-         
-        p1.invierteSimple();
+        p1.push(10);
         
-        System.out.println(p1.verElemento());
+       
+
+       // p1.invierteSimple(p1, p2, p3);
+      //p1.invierteEficiente(p1, p3);
       
-//       System.out.println("El elemento en el tope es: " + p1.verElemento());
-//        System.out.println("La pila esta vacia?" + p1.pilaVacia());
-        
-        
+        //System.out.println(p1.verElemento());
+       p1.invierteEficiente(p1, p2);
+       p1.MostrarPila();   
+                
     }
-    
-    
     
 }
