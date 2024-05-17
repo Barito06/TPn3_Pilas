06bc578c4a3676aae85092afc0fc5f6d099bcc37
Barito06 <aguerocalderonhectorjoaquin@gmail.com>
2024-05-17 19:33:45 -0300

    volver a version anterior

--- src/tpn3_pilas/TPn3_Pilas.java
+++ src/tpn3_pilas/TPn3_Pilas.java
@@ -2,43 +2,43 @@ package tpn3_pilas;
 public class TPn3_Pilas {
 
     public static void main(String[] args) {
-        Pila p1 = new Pila(10);
-        Pila p2 = new Pila(10);
-        Pila p3 = new Pila(10);
-        Pila p4 = new Pila(6);
-        Pila p5 = new Pila(6);
-        Pila p6 = new Pila(6);
-
-        p1.push(1);        
-        p1.push(2);
-        p1.push(3);
-        p1.push(4);
-        p1.push(5);
-        p1.push(6);
-        p1.push(7);
-        p1.push(8);
-        p1.push(9);
-        p1.push(10);
-        
-        //ejercicio d del practico tp3 parte 2
-        p4.push(9);
-        p4.push(5);
-        p4.push(3);
-        
-        p5.push(10);
-        p5.push(9);
-        p5.push(7);
-        
-       
-
-       // p1.invierteSimple(p1, p2, p3);
-      //p1.invierteEficiente(p1, p3);
-      
-        //System.out.println(p1.verElemento());
-       //p1.invierteEficiente(p1, p2);
-       p6.ordenarMayorAMenor(p4, p5, p6);
-                       p6.MostrarPila();   
-
+//        Pila p1 = new Pila(10);
+//        Pila p2 = new Pila(10);
+//        Pila p3 = new Pila(10);
+//        Pila p4 = new Pila(6);
+//        Pila p5 = new Pila(6);
+//        Pila p6 = new Pila(6);
+//
+//        p1.push(1);        
+//        p1.push(2);
+//        p1.push(3);
+//        p1.push(4);
+//        p1.push(5);
+//        p1.push(6);
+//        p1.push(7);
+//        p1.push(8);
+//        p1.push(9);
+//        p1.push(10);
+//        
+//        //ejercicio d del practico tp3 parte 2
+//        p4.push(9);
+//        p4.push(5);
+//        p4.push(3);
+//        
+//        p5.push(10);
+//        p5.push(9);
+//        p5.push(7);
+//        
+//       
+//
+//       // p1.invierteSimple(p1, p2, p3);
+//      //p1.invierteEficiente(p1, p3);
+//      
+//        //System.out.println(p1.verElemento());
+//       //p1.invierteEficiente(p1, p2);
+//       p6.ordenarMayorAMenor(p4, p5, p6);
+//                       p6.MostrarPila();   
+ // Vamos a probar renovar los cambios hechos...
     }
     
 }
